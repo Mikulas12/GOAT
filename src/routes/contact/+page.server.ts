5af4002363c87832f4dbc5b0833de3ac1418b7dc
Mikulas12 <mmerhulik@gmail.com>
2025-06-30 19:31:11 +0200

    oprava 3

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,10 +1,8 @@
-// src/routes/kontakt/+page.server.ts
+// src/routes/contact/+page.server.ts
 import type { PageServerLoad } from './$types';
 
-// Vypne statický prerender pro tuto stránku
 export const prerender = false;
 
-// Žádné volání na Prismic, vracíme prázdný objekt
 export const load: PageServerLoad = async () => {
   return {};
 };
