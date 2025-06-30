62c095f32bbde0ebc1a9986dc076ff5dc090bbeb
Mikulas12 <mmerhulik@gmail.com>
2025-06-30 19:07:58 +0200

    Fix breaking build

--- src/routes/sluzby/+page.svelte
+++ src/routes/sluzby/+page.svelte
@@ -16,42 +16,42 @@
       description: 'Interaktivní 3D prohlídky interiérů ve vašem prohlížeči.',
       fullDescription:
         'Naše VR zážitky umožňují plné ponoření do prostoru: podpora kontrolerů, interaktivní prvky a možnost brandingu.',
-      icon: '/static/vr.jpg'
+      icon: '/vr.jpg'
     },
     {
       title: '360° Panorama',
       description: 'Plně sférické panoramatické záběry pro dokonalý pohled.',
       fullDescription:
         '360° panoramata generujeme z fotografií nebo 3D modelů, s podporou hotspotů a textových popisků.',
-      icon: '/static/360.jpg'
+      icon: '/360.jpg'
     },
     {
       title: 'Procházkový režim',
       description: 'Volné pohybování ve virtuálních prostorech s intuitivním ovládáním.',
       fullDescription:
         'Navrhneme trasu, optimalizujeme kolize, přidáme navigační UI, mapu a režim „teleportu“ pro VR headsety.',
-      icon: '/static/1.png'
+      icon: '/1.png'
     },
     {
       title: '3D modelování',
       description: 'Návrh a vizualizace objektů v Blenderu a Rhinoceros.',
       fullDescription:
         'Od konceptového modelu přes CAD výkresy až po export pro real-time aplikace a 3D tisk.',
-      icon: '/static/stul.png'
+      icon: '/stul.png'
     },
     {
       title: 'Návrh loga & značky',
       description: 'Tvorba silné vizuální identity, logotypu a komplexního brandingu.',
       fullDescription:
         'Dodáme kompletní brand manuál: barvy, typografii, ikonografii a příklady použití v tiskovinách i online.',
-      icon: '/static/logo.jpg'
+      icon: '/logo.jpg'
     },
     {
       title: 'Web & aplikace',
       description: 'Full-stack vývoj (React, Svelte), nasazení a SEO optimalizace.',
       fullDescription:
         'Vytvoříme responzivní weby a aplikace, integrujeme 3D/WebGL prvky, zajistíme CI/CD a monitoring.',
-      icon: '/static/programing.jpg'
+      icon: '/programing.jpg'
     }
   ];
 
