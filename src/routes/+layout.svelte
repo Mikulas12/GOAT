1a3674cbdde3eaf617c413c577b02d1d9604306e
Mikulas12 <mmerhulik@gmail.com>
2025-07-26 16:53:07 +0200

    fix: odkomentovat layout a odstranit injectAnalytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,33 @@
 
   onMount(() => {
     if (browser) {
-      
+      // zde můžeš mít vlastní JS, když bude potřeba
     }
   });
 </script>
+
+<svelte:head>
+  <title>{$page.data.title}</title>
+  {#if $page.data.meta_description}
+    <meta name="description" content={$page.data.meta_description} />
+  {/if}
+  {#if $page.data.meta_title}
+    <meta name="og:title" content={$page.data.meta_title} />
+  {/if}
+  {#if $page.data.meta_image}
+    <meta name="og:image" content={$page.data.meta_image} />
+    <meta name="twitter:card" content="summary_large_image" />
+  {/if}
+</svelte:head>
+
+<Header settings={data.settings} />
+<main>
+  <slot />
+</main>
+<div class="background-gradient absolute inset-0 -z-50 max-h-screen" />
+<div
+  class="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"
+></div>
+<Footer settings={data.settings} />
+<PrismicPreview {repositoryName} />
+
