cb869967c8686959f1110ccc0fab549f40619f0d
Mikulas12 <mmerhulik@gmail.com>
2025-07-26 16:00:26 +0200

    halo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,26 @@
-<script>
-	import '../app.css';
-	import '@fontsource-variable/urbanist';
+<script lang="ts">
+  import '../app.css';
+  import '@fontsource-variable/urbanist';
 
-	import { PrismicPreview } from '@prismicio/svelte/kit';
-	import { page } from '$app/stores';
-	import { repositoryName } from '$lib/prismicio';
-	import Footer from '$lib/components/Footer.svelte';
-	import Header from '$lib/components/Header.svelte';
+  import { onMount } from 'svelte';
+  import { browser } from '$app/environment';
+  import { injectAnalytics } from '@vercel/analytics';
 
-	export let data;
+  import { PrismicPreview } from '@prismicio/svelte/kit';
+  import { page } from '$app/stores';
+  import { repositoryName } from '$lib/prismicio';
+  import Footer from '$lib/components/Footer.svelte';
+  import Header from '$lib/components/Header.svelte';
+
+  export let data;
+
+  onMount(() => {
+    if (browser) {
+      injectAnalytics();
+    }
+  });
 </script>
+
 
 <svelte:head>
 	<title>{$page.data.title}</title>
